75a179087ac22278784de99564f4809ce65facbb
Rona Chong <ronachong@gmail.com>
2016-02-29 17:16:19 -0800

    Embed toggle_status function in window.onload function.

--- post_a_status.js
+++ post_a_status.js
@@ -1,31 +1,14 @@
-//window.onload = function () {
-//    document.getElementById('status_form').style.display = 'none';
-//    document.getElementById('post_link').style.display = 'block';
-//};
-
-
-
-window.onload = function () {
+window.onload = function toggle_status(){
     var status_form = document.getElementById('status_form');
     var post_link = document.getElementById('post_link');
 
 
-    post_link.onclick = function toggle_status() {
+    post_link.onclick = function () {
         if (status_form.style.display == 'none') {
             status_form.style.display = 'block';
         } else {
             status_form.style.display = 'none';
         }
     };
-};
 
-//document.getElementById('post_link').onclick = function () {
-//    document.getElementById('status_form').style.display = 'none';
-//};
-
-//var post_link = document.getElementById('post_link');
-//var status_form = document.getElementById('status_form');
-
-//post_link.onclick = function() {
-//    status_form.style.display = 'none';
-//}
+}
