54a5ac482324eff6420a2fa5a14242a4497e5b05
Rona Chong <ronachong@gmail.com>
2016-03-02 13:11:37 -0800

    Move variable initialization up to top of reply.js. Change reply_links to reply_links_array. Add if conditional to change display property of reply_form.

--- reply.js
+++ reply.js
@@ -1,11 +1,15 @@
 function toggle_reply() {
-    var reply_links = document.querySelectorAll('.reply_link');
+    var reply_links_array = document.querySelectorAll('.reply_link');
+    var reply_id = null; //will be assigned val later
+    var reply_form = null;
     
-    for (var i = 0; i < reply_links.length; i++) {
-        reply_links[i].addEventListener("click", function () {
-            var reply_id = this.getAttribute("data-form_no");
-            document.getElementById(reply_id).style.display = 'block';
-            
+    for (var i = 0; i < reply_links_array.length; i++) {
+        reply_links_array[i].addEventListener("click", function () {
+            reply_id = this.getAttribute("data-form_no");
+            reply_form = document.getElementById(reply_id);
+            if (reply_form.style.display == "none" || reply_form.style.display == "") {
+                reply_form.style.display = 'block';
+            }
         })                                    
     }
 }
