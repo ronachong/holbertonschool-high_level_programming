4c32759ba7a634897e8d213c875aae0f83e57475
Kris Bredemeier <34@holbertonschool.com>
2016-03-18 17:27:23 -0700

    creates callback function for ajaxGet

--- load_more.js
+++ load_more.js
@@ -1,4 +1,22 @@
-function loadDoc() {
+window.onload = function load_more() {
+    var see_more = document.getElementById('see_more');
+    var makeResponseText = function (string) {
+      document.getElementById("demo").innerHTML = string;
+    }
+
+    see_more.onclick = function () {
+      var timeDelay = 2000;
+      var ajaxRequest = ajaxGet("/statuses-1.html", makeResponseText);
+
+      setTimeout(ajaxRequest, timeDelay);
+  }
+}
+
+
+
+
+
+/* function loadDoc() {
   var xhttp = new XMLHttpRequest();
   xhttp.onreadystatechange = function() {
     if (xhttp.readyState == 4 && xhttp.status == 200) {
@@ -7,4 +25,4 @@ function loadDoc() {
   };
   xhttp.open("GET", "/statuses-1.html", true);
   xhttp.send();
-}
+} */
