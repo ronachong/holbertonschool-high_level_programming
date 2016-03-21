186e985f28f8d9c3a35dfec268f5aaa6358a6a54
Rona Chong <ronachong@gmail.com>
2016-03-20 19:22:38 -0700

    Move definition of anonymous function into var doStuff for setTimeout call.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,7 +1,6 @@
 function ajaxGet(url, onSuccess) {
-
-    setTimeout(function () {
-        var xhttp = new XMLHttpRequest();
+    doStuff = function (url, onSuccess) {
+      var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 onSuccess(xhttp.responseText);
@@ -9,7 +8,9 @@ function ajaxGet(url, onSuccess) {
         };
         xhttp.open("GET", url, true);
         xhttp.send();
-    }, 2000);
+    };
+    
+    setTimeout(doStuff, 2000, url, onSuccess);
 }
 
 
