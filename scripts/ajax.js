57052810d17e87090d76067e32a25255a900d985
Rona Chong <ronachong@gmail.com>
2016-03-20 20:01:48 -0700

    Add extra toggle_reply code to ajaxGet function, with selectors specific to status-1 html.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,6 +1,8 @@
 function ajaxGet(url, onSuccess) {
-    doStuff = function (url, onSuccess) {
-      var xhttp = new XMLHttpRequest();
+    
+    // doStuff makes an Ajax request and reruns script for toggle_reply
+    var doStuff = function (url, onSuccess) {
+        var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 onSuccess(xhttp.responseText);
@@ -8,6 +10,21 @@ function ajaxGet(url, onSuccess) {
         };
         xhttp.open("GET", url, true);
         xhttp.send();
+
+        
+        var more_reply_links_array = document.querySelectorAll('#more_statuses .reply_link');
+        var reply_id = null; //will be assigned val later
+        var reply_form = null;
+
+        for (var i = 0; i < more_reply_links_array.length; i++) {
+            more_reply_links_array[i].addEventListener("click", function () {
+                console.log("I see this click.");
+                reply_id = this.getAttribute("data-form_no");
+                reply_form = document.getElementById(reply_id);
+                reply_form.toggle();
+                this.style.display = "none"; //this is still the reply link
+            })                                    
+        }
     };
     
     setTimeout(doStuff, 2000, url, onSuccess);
