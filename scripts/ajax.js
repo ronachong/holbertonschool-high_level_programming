1cc9370eceb08155ee6d80d52991925ecc0ea278
Rona Chong <ronachong@gmail.com>
2016-03-20 22:45:54 -0700

    Embed call to toggle_reply_again inside ajax call (ensures that toggle_reply runs only when DOM is finished loading new elements.

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -1,30 +1,43 @@
+var toggle_reply_again = function () {
+    var more_reply_links_array = document.querySelectorAll('#more_statuses .reply_link');
+    console.log("Made new array for new reply forms.");
+    console.log("New reply forms are stored in " + more_reply_links_array);
+    var reply_id = null; //will be assigned val later
+    var reply_form = null;
+    
+    more_reply_links_array[0].addEventListener("click", function () {
+        console.log("I see you doing THIS.");
+    });
+
+    for (var i = 0; i < more_reply_links_array.length; i++) {
+        console.log("Loop thru array.");
+        
+        
+        more_reply_links_array[i].addEventListener("click", function () {
+            console.log("I see this click.");
+            reply_id = this.getAttribute("data-form_no");
+            reply_form = document.getElementById(reply_id);
+            reply_form.toggle();
+            this.style.display = "none"; //this is still the reply link
+        })
+    }
+}
+
 function ajaxGet(url, onSuccess) {
     
-    // doStuff makes an Ajax request and reruns script for toggle_reply
+    // doStuff makes an Ajax request and runs script for toggling of more replies
     var doStuff = function (url, onSuccess) {
         var xhttp = new XMLHttpRequest();
         xhttp.onreadystatechange = function () {
             if (xhttp.readyState == 4 && xhttp.status == 200) {
                 onSuccess(xhttp.responseText);
+                toggle_reply_again();
             }
         };
         xhttp.open("GET", url, true);
         xhttp.send();
-
         
-        var more_reply_links_array = document.querySelectorAll('#more_statuses .reply_link');
-        var reply_id = null; //will be assigned val later
-        var reply_form = null;
-
-        for (var i = 0; i < more_reply_links_array.length; i++) {
-            more_reply_links_array[i].addEventListener("click", function () {
-                console.log("I see this click.");
-                reply_id = this.getAttribute("data-form_no");
-                reply_form = document.getElementById(reply_id);
-                reply_form.toggle();
-                this.style.display = "none"; //this is still the reply link
-            })                                    
-        }
+        document.getElementById("see_more").style.display = 'none';
     };
     
     setTimeout(doStuff, 2000, url, onSuccess);
