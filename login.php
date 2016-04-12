50c866b0b88cf648c6d2168ae1403f9301552060
Rona Chong <ronachong@gmail.com>
2016-04-12 02:29:01 +0000

    Copy index.html into login.php; remove all but select header elements; add login/password form.

---
+++ login.php
@@ -0,0 +1,50 @@
+<!doctype html>
+
+<html lang="en">
+    <head>
+        <!-- Meta -->
+        <meta charset="utf-8">
+        <title>impossible octopus fitness</title>
+        <meta name="description" content="impossible octopus fitness">
+        <meta name="Kris and Rona" content="impossible octopus fitness">
+        <meta name="viewport" content="width=760, initial-scale=1.0">
+
+        <!-- Style References -->
+        <link href='https://fonts.googleapis.com/css?family=Gochi+Hand' rel='stylesheet' type='text/css'>
+        <link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro' rel='stylesheet' type='text/css'>
+        <link rel="stylesheet" type="text/css" href="style.css">
+
+        <!-- Javascript -->
+        <script src="scripts/post_a_status.js"></script>
+        <script src="scripts/reply.js"></script>
+        <script src="scripts/toggle.js"></script>
+        <script src="scripts/ajax.js"></script>
+        <script src="scripts/load_more.js"></script>
+    </head>
+
+
+    <body>
+        <div id="container">
+
+            <!-- HEADER -->
+            <header>
+
+                <!-- actual header -->
+                <div id="header_block">
+                        <img id="warbler_logo" width="100" src="http://www.procliparts.com/cliparts/pele/ekiE9gTAo-orange-bird.png" alt="log">
+                        <h1>Warbler</h1>
+                        <p id="tag_line">Home of Impossible Octopus Fitness.</p>
+			
+			<form>
+			  Login: 
+			  <input type="text" name="username field"><br>
+			  Password: 
+			  <input type="text" name="password field">
+			</form>
+                </div>
+
+            </header>
+
+        </div>
+    </body>
+</html>
